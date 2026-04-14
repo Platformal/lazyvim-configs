fec992c86f90a076c7b23f09a9d4ba094aa0dc4a
Frank Doan <tullskon@gmail.com>
2026-04-13 22:25:15 -0400

    Updated Arctic theme to V2 and included debug adapter protocol for python, and may (not) work on Linux.

--- init.lua
+++ init.lua
@@ -1,2 +1,14 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+local dap, dapui = require("dap"), require("dapui")
+dapui.setup()
+dap.listeners.after.event_initialized["dapui_config"] = function()
+  dapui.open()
+end
+dap.listeners.before.event_terminated["dapui_config"] = function()
+  dapui.close()
+end
+dap.listeners.before.event_exited["dapui_config"] = function()
+  dapui.close()
+end
