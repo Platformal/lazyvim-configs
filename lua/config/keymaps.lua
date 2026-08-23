7f55c64afd448d61ae31812d304e5bc9e83c265c
Frank Doan <tullskon@gmail.com>
2026-08-23 10:50:06 -0400

    Removed unused block.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,16 +1,4 @@
 -- Format and save buffer
--- vim.keymap.set({ "n", "i" }, "<C-s>", function()
---   vim.lsp.buf.code_action({
---     context = {
---       only = { "source.organizeImports.ruff" },
---     },
---     apply = true,
---   })
-
---   vim.cmd("LazyFormat")
---   vim.cmd("write")
--- end, { desc = "Organize imports, format, and save" })
-
 vim.keymap.set({ "n", "i" }, "<C-s>", function()
   require("conform").format({
     async = false,
